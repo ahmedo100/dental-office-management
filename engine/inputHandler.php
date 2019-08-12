b16e8a84a7103c4de484d60b3c6a7d92c34a742f
Baoun Ahmed Nadir <lazertu4@gmail.com>
2019-08-12 20:57:52 +0000

    Update inputHandler.php

--- engine/inputHandler.php
+++ engine/inputHandler.php
@@ -3,11 +3,13 @@ require_once 'database-logs.php';
 require_once 'database-functions.php';
 
 class InputHandler {
+    private $indexArray = [];
     private $valuesArray =[];
     private $db ;
 
-    public function __construct($valuesArray){
+    public function __construct($indexArray,$valuesArray){
         $db = new DatabaseUtility(SERVERNAME,USERNAME,PASSWORD);
+        $this->indexArray = $indexArray;
         $this->valuesArray =$valuesArray;
     }
 
@@ -21,11 +23,13 @@ class InputHandler {
         return $emptyIndex;
     }
 
+        
     public function insertInputs($tableName){
-       $return_result =  $db->insertData($tableName,$valuesArray);
-
-       if($return_result["success"]!=true){
-           print_r($return_result);
-       }
-    }
+        $return_result =  $db->insertData($tableName,$valuesArray);
+ 
+        if($return_result["success"]!=true){
+            print_r($return_result);
+        }
+     }
+    
 }
