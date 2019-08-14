415bab476f942ea38056b8e7daebb93ff59ba342
Baoun Ahmed Nadir <lazertu4@gmail.com>
2019-08-14 15:14:43 +0000

    removed appointment.sql , added new sql file

--- dental_office_db.sql
+++ dental_office_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 07, 2019 at 06:09 PM
+-- Generation Time: Aug 14, 2019 at 05:14 PM
 -- Server version: 10.1.35-MariaDB
 -- PHP Version: 7.2.9
 
@@ -35,6 +35,35 @@ CREATE TABLE `appointment` (
   `timestamp_appointment` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctors`
+--
+
+CREATE TABLE `doctors` (
+  `id_doctor` int(8) NOT NULL,
+  `full_name_doctor` varchar(128) NOT NULL,
+  `gender_doctor` bit(1) NOT NULL,
+  `phone_number_doctor` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patients`
+--
+
+CREATE TABLE `patients` (
+  `id_patient` int(8) NOT NULL,
+  `id_doctor` int(8) NOT NULL,
+  `full_name_patient` varchar(128) NOT NULL,
+  `birthdate_patient` date NOT NULL,
+  `phone_number_patient` int(12) NOT NULL,
+  `address_patient` varchar(256) NOT NULL,
+  `gender_patient` bit(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexes for dumped tables
 --
@@ -48,6 +77,19 @@ ALTER TABLE `appointment`
   ADD KEY `id_patient` (`id_patient`);
 
 --
+-- Indexes for table `doctors`
+--
+ALTER TABLE `doctors`
+  ADD PRIMARY KEY (`id_doctor`);
+
+--
+-- Indexes for table `patients`
+--
+ALTER TABLE `patients`
+  ADD PRIMARY KEY (`id_patient`),
+  ADD KEY `id_doctor` (`id_doctor`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -56,6 +98,18 @@ ALTER TABLE `appointment`
 --
 ALTER TABLE `appointment`
   MODIFY `id_appointment` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `doctors`
+--
+ALTER TABLE `doctors`
+  MODIFY `id_doctor` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `patients`
+--
+ALTER TABLE `patients`
+  MODIFY `id_patient` int(8) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -67,6 +121,12 @@ ALTER TABLE `appointment`
 ALTER TABLE `appointment`
   ADD CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`id_doctor`) REFERENCES `doctors` (`id_doctor`),
   ADD CONSTRAINT `appointment_ibfk_2` FOREIGN KEY (`id_patient`) REFERENCES `patients` (`id_patient`);
+
+--
+-- Constraints for table `patients`
+--
+ALTER TABLE `patients`
+  ADD CONSTRAINT `patients_ibfk_1` FOREIGN KEY (`id_doctor`) REFERENCES `doctors` (`id_doctor`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
